34acf2615c5588cd56c55fb55c7a0bb635dc7bfc
Ranjan <ranjancse@gmail.com>
2016-01-06 22:24:09 -0600

    Initial Checkin

---
+++ TrueMedLib/MedicineSuggestion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace TrueMedLib
+{
+    public class Suggestion
+    {
+        public string suggestion { get; set; }
+    }
+
+    public class MedicineSuggestion
+    {
+        public List<Suggestion> suggestions { get; set; }
+    }
+}
